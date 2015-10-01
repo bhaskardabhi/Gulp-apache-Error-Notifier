a691314f47d36256763a3bfc61840f7bf8b9e886
Bhaskar Dabhi <bhaskar.dabhi@plus91.in>
2015-10-01 18:09:51 +0530

    Added Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@ var notify = require("gulp-notify");
 var intercept = require('gulp-intercept');
 var lastErrorTime = null;
 var myRegx = /\[(.*?)\] \[(.*?)\] \[(.*?)\] \[(.*?)\]/i;
+var logPath = "/var/log/apache2/error.log";
 
 gulp.task('default', ['watch']);
 
 gulp.task('watch', function() {
 
-	gulp.watch(['/var/log/apache2/error.log']).on('change', function(file) {
+	gulp.watch([logPath]).on('change', function(file) {
 		gulp.src([file.path])
 		    .pipe(intercept(function(fileScaned){
 		    	var errorLines = fileScaned.contents.toString().split('\n'),
