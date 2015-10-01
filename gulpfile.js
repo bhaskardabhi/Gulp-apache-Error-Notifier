ac69dbf626a9117e7088d2e32b1cd579d7ac583f
Bhaskar Dabhi <bhaskar.dabhi@plus91.in>
2015-10-01 18:01:09 +0530

    Initialized Git Repo

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var notify = require("gulp-notify");
+var intercept = require('gulp-intercept');
+var lastErrorTime = null;
+var myRegx = /\[(.*?)\] \[(.*?)\] \[(.*?)\] \[(.*?)\]/i;
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+
+	gulp.watch(['/var/log/apache2/error.log']).on('change', function(file) {
+		gulp.src([file.path])
+		    .pipe(intercept(function(fileScaned){
+		    	var errorLines = fileScaned.contents.toString().split('\n'),
+		    		errorTempLines = [],
+		    		displayLines = [],
+		    		lastDisplayLines = 1;
+
+		    	for (var i = 0; i < errorLines.length; i++) {
+		    		if(errorLines[i].indexOf("PHP Fatal error:") > -1){
+		    			errorTempLines.push(errorLines[i]);
+		    		}
+		    	};
+
+		    	if(errorTempLines.length <= lastDisplayLines){
+		    		displayLines = errorTempLines;
+		    	} else {
+			    	for (var i = 0; i < lastDisplayLines; i++) {
+			    		var error = errorTempLines[errorTempLines.length - lastDisplayLines - i],
+			    			errorArray = error.split("PHP Fatal error:"),
+			    			errorTime = myRegx.exec(errorArray[0])[1],
+			    			errorMsg = errorArray[1];
+
+			    		if(lastErrorTime != errorTime){
+				    		displayLines.push(errorMsg);
+				    		
+				    		lastErrorTime = errorTime;
+			    		}
+			    	};
+		    	}
+
+		    	if(displayLines.length){
+		    		gulp.src(file.path).pipe(notify("Time: "+lastErrorTime+"\n "+ displayLines.join("\n\n")));
+		    	}
+		    }));
+    })
+});
